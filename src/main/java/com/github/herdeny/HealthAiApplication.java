76a02f655fc510b5426647d9ec0270cbc856a269
Herdeny <59501387+Herdeny@users.noreply.github.com>
2024-10-09 12:02:21 +0800

    Update HealthAiApplication.java
    指定端口

--- src/main/java/com/github/herdeny/HealthAiApplication.java
+++ src/main/java/com/github/herdeny/HealthAiApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HealthAiApplication {
 
+	@Value("${server.port}")
+ 	private int port;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HealthAiApplication.class, args);
 	}
