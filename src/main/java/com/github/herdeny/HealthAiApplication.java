97900dd64b046df0d4b5ccfa688069e4d0845b4e
YANGYK6 <121705897+YANGYK6@users.noreply.github.com>
2024-10-14 22:55:44 +0800

    Update HealthAiApplication.java

--- src/main/java/com/github/herdeny/HealthAiApplication.java
+++ src/main/java/com/github/herdeny/HealthAiApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HealthAiApplication {
 
-	@Value("${server.port}")
- 	private int port;
-
 	public static void main(String[] args) {
 		SpringApplication.run(HealthAiApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(HealthAiApplication.class);
 	}
 
 }
