7269cc179bc00f5b600cc1e7540b405da18ed9d3
YANGYK6 <121705897+yangyk6@users.noreply.github.com>
2024-10-15 16:01:41 +0800

    Update HealthAiApplication.java

--- src/main/java/com/github/herdeny/HealthAiApplication.java
+++ src/main/java/com/github/herdeny/HealthAiApplication.java
@@ -2,9 +2,11 @@ package com.github.herdeny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HealthAiApplication {
+public class HealthAiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthAiApplication.class, args);
