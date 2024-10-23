06bbfd8a31838744156818af5ce2e0e2cfad11e7
Herdeny <1653146952@qq.com>
2024-10-23 23:05:42 +0800

    合并更改

--- src/main/java/com/github/herdeny/controller/ADGRN_Controller.java
+++ src/main/java/com/github/herdeny/controller/ADGRN_Controller.java
@@ -2,17 +2,21 @@ package com.github.herdeny.controller;
 
 import com.github.herdeny.pojo.Result;
 import com.github.herdeny.service.ADGRN_Service;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+
 @RestController
 @RequestMapping("/adgrn")
-public class ADGRN_Controller {
+public class ADGRN_Controller extends CommonController{
+
     @Autowired
     private ADGRN_Service adgrnService;
 
-    @Value("${PROJECT_PATH}")
+    @Value("${DATA_PATH}")
     private static String DIRECTORY;
 
     System.Logger logger = System.getLogger(ADGRN_Controller.class.getName());
@@ -57,6 +61,21 @@ public class ADGRN_Controller {
     public Result img(String filePath) {
         adgrnService.adgrn_createImg(filePath);
         return Result.success();
+    }
+
+    /**
+     * 获取GRN图
+     * @param response
+     */
+    @GetMapping("/getGRN")
+    public void getAvatar(HttpServletResponse response) {
+        String GRNFolderPath = DIRECTORY + "GRN.png";
+        // 判断GRN路径是否存在
+        while (!new File(GRNFolderPath).exists()) {
+            return;
+        }
+        response.setContentType("image/jpeg");
+        readFile(response, GRNFolderPath);
     }
 
 }
