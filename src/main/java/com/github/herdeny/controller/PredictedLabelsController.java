c196c4fecbfab0a238af688d79209fa52f98bd2b
Herdeny <1653146952@qq.com>
2024-10-28 19:40:00 +0800

    数据路径重复  删除此处的数据路径

--- src/main/java/com/github/herdeny/controller/PredictedLabelsController.java
+++ src/main/java/com/github/herdeny/controller/PredictedLabelsController.java
@@ -38,53 +38,8 @@ public class PredictedLabelsController {
 
     @PostMapping("/run")
     public Result run(@RequestParam String fileName) {
-        String filePath = DIRECTORY + "/" + fileName;
-        String result = predictedLabelsService.predict(filePath);
+        String result = predictedLabelsService.predict(fileName);
         return Result.success(result);
     }
-
- /*   @GetMapping(value = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter stream(@RequestBody Query query) {
-        String filePath = query.getData();
-        SseEmitter sseEmitter = new SseEmitter();
-        Executors.newSingleThreadExecutor().execute(() -> {
-            Process process = null;
-            try {
-                String[] args1 = new String[]{
-                        pythonPath, GeneAnalysisPath, filePath,
-                        final_spatial_fusion, label_mapping,
-                        best_model_tempro_spatialfusion_Braak,
-                        best_model_tempro_spatialfusion_CERAD,
-                        best_model_tempro_spatialfusion_Cogdx
-                };
-                process = Runtime.getRuntime().exec(args1);
-                // 读取进程的标准输出流
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8))) {
-                    String line;
-                    while ((line = in.readLine()) != null) {
-                        System.out.println(line); // 输出到控制台
-                        sseEmitter.send(line);     // 发送到前端
-                    }
-                }// 等待进程结束
-                int exitCode = process.waitFor();
-                if (exitCode != 0) {
-                    sseEmitter.completeWithError(new RuntimeException("Process exited with code: " + exitCode));
-                }
-
-                sseEmitter.complete(); // 完成发送
-            } catch (Exception e) {
-                try {
-                    sseEmitter.completeWithError(e); // 发送错误到前端
-                } catch (Exception e1) {
-                    e1.printStackTrace();
-                }
-            } finally {
-                if (process != null) {
-                    process.destroy(); // 清理进程
-                }
-            }
-        });
-        return sseEmitter;
-    }*/
 
 }
