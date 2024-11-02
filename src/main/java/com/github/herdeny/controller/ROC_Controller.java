41979d5d5383799a2f2b520e6dcc842901a77b69
Herdeny <1653146952@qq.com>
2024-11-02 13:39:04 +0800

    新增获取ROC图的接口

--- src/main/java/com/github/herdeny/controller/ROC_Controller.java
+++ src/main/java/com/github/herdeny/controller/ROC_Controller.java
@@ -2,14 +2,17 @@ package com.github.herdeny.controller;
 
 import com.github.herdeny.pojo.Result;
 import com.github.herdeny.service.ROC_Service;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+
 
 @RestController
 @RequestMapping("/ROC")
-public class ROC_Controller {
+public class ROC_Controller extends CommonController {
 
     @Autowired
     private ROC_Service rocService;
@@ -22,9 +25,28 @@ public class ROC_Controller {
 
     System.Logger logger = System.getLogger(PredictedLabelsController.class.getName());
 
+
+    //TODO 返回值
     @PostMapping("/test")
     public Result test(@RequestParam String test_code) {
         String result = rocService.test(test_code);
         return Result.success("test completed" + result);
+    }
+
+
+    /**
+     * 获取ROC图
+     *
+     * @param response
+     */
+    @GetMapping("/getROC")
+    public void getROC(HttpServletResponse response) {
+        String ROCFolderPath = DATA_PATH + "ROC.png";
+        // 判断路径是否存在
+        if (!new File(ROCFolderPath).exists()) {
+            return;
+        }
+        response.setContentType("image/png");
+        readFile(response, ROCFolderPath);
     }
 }
